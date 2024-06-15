e6e825e64dd06e1c46290b3925d4b3df8f774113
DiscreteTom <discrete_tom@outlook.com>
2024-06-15 21:42:39 +0800

    chore(deck): add scripts/setup.sh

---
+++ deck/scripts/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp ./libsteam_api.so /usr/lib
+mkdir -p /home/deck/.local/share/Steam/controller_config
+cp ./stickdeck.vdf /home/deck/.local/share/Steam/controller_config/game_actions_480.vdf
