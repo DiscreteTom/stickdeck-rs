4faf9d260dd52ba7885b7ba7b40a8d58a999071a
DiscreteTom <discrete_tom@outlook.com>
2024-06-15 22:36:39 +0800

    fix(deck): update scripts/setup.sh, use sudo

--- deck/scripts/setup.sh
+++ deck/scripts/setup.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-
-cp ./libsteam_api.so /usr/lib
-mkdir -p /home/deck/.local/share/Steam/controller_config
-cp ./stickdeck.vdf /home/deck/.local/share/Steam/controller_config/game_actions_480.vdf
+konsole -e "sudo cp ./libsteam_api.so /usr/lib && mkdir -p /home/deck/.local/share/Steam/controller_config && cp ./stickdeck.vdf /home/deck/.local/share/Steam/controller_config/game_actions_480.vdf"
