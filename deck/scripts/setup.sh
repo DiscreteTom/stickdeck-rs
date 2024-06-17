b9917c5098ac89305f093d88b83c6e8e9b556b3b
DiscreteTom <discrete_tom@outlook.com>
2024-06-17 21:14:04 +0800

    fix(deck): fix setup.sh

--- deck/scripts/setup.sh
+++ deck/scripts/setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-konsole -e "mkdir -p /home/deck/.local/share/Steam/controller_config && cp ./stickdeck.vdf /home/deck/.local/share/Steam/controller_config/game_actions_480.vdf"
+konsole -e "/bin/bash -c 'mkdir -p /home/deck/.local/share/Steam/controller_config && cp ./stickdeck.vdf /home/deck/.local/share/Steam/controller_config/game_actions_480.vdf'"
