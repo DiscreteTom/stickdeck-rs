f519e21438bebe79682f84dc571ecc70fae0fabc
DiscreteTom <discrete_tom@outlook.com>
2024-06-17 20:20:34 +0800

    feat(deck): add launch.sh so that user don't need sudo when setup

--- deck/scripts/debug.sh
+++ deck/scripts/debug.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-konsole -e "/bin/bash -c './stickdeck || read -p \"Press enter to exit\"'"
+konsole -e "/bin/bash -c 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:. && (./stickdeck || true) && read -p \"Press enter to exit\"'"
--- deck/scripts/setup.sh
+++ deck/scripts/setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-konsole -e "sudo cp ./libsteam_api.so /usr/lib && mkdir -p /home/deck/.local/share/Steam/controller_config && cp ./stickdeck.vdf /home/deck/.local/share/Steam/controller_config/game_actions_480.vdf"
+konsole -e "mkdir -p /home/deck/.local/share/Steam/controller_config && cp ./stickdeck.vdf /home/deck/.local/share/Steam/controller_config/game_actions_480.vdf"
