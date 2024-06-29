5b8137b4f3ff08e473e70aa1d5057c7db6356acd
DiscreteTom <discrete_tom@outlook.com>
2024-06-29 16:01:17 +0800

    chore(win): replace dependencies use windows-rs instead of mouse-rs because mouse-rs is not stable in release build

--- win/src/win.rs
+++ win/src/win.rs
@@ -1,10 +1,13 @@
 mod client;
 
 use log::{debug, info, log_enabled, trace, Level};
-use mouse_rs::Mouse;
 use std::{env, sync::mpsc, time::Instant};
 use stickdeck_common::{MouseMove, Packet};
 use vigem_client::{Client, TargetId, XGamepad, Xbox360Wired};
+use windows::Win32::{
+  Foundation::POINT,
+  UI::WindowsAndMessaging::{GetCursorPos, SetCursorPos},
+};
 
 fn main() {
   if env::var("RUST_LOG").is_err() {
@@ -28,7 +31,7 @@ fn main() {
   );
 
   let mut update_controller = init_controller();
-  let move_mouse = init_mouse();
+  let mut move_mouse = init_mouse();
 
   let mut now = Instant::now();
   let mut count = 0;
@@ -79,15 +82,11 @@ fn init_controller() -> impl FnMut(&XGamepad) {
   }
 }
 
-fn init_mouse() -> impl Fn(&MouseMove) {
-  let mouse = Mouse::new();
+fn init_mouse() -> impl FnMut(&MouseMove) {
+  let mut pos = POINT::default();
 
-  move |data| {
-    let pos = mouse
-      .get_position()
-      .expect("Failed to get the mouse position");
-    mouse
-      .move_to(pos.x + data.x as i32, pos.y + data.y as i32)
-      .expect("Failed to move the mouse");
+  move |data: &MouseMove| unsafe {
+    GetCursorPos(&mut pos).expect("Failed to get the cursor position");
+    SetCursorPos(pos.x + data.x as i32, pos.y + data.y as i32).expect("Failed to move the mouse");
   }
 }
