8545ade4b547f5801d2c937420dd994da7abf499
DiscreteTom <discrete_tom@outlook.com>
2025-12-23 19:46:50 +0800

    chore(common): more comment

--- common/src/gamepad/mod.rs
+++ common/src/gamepad/mod.rs
@@ -1,3 +1,6 @@
-mod serializable;
+//! The GamePad struct in the server side and the client side
+//! are 2 different struct, so we use macros to define common behaviors.
+
 mod deserializable;
+mod serializable;
 mod tests;
