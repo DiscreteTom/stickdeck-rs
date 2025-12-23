3bffc52fed280fa09998ca2e921a709c7469900d
DiscreteTom <discrete_tom@outlook.com>
2025-12-23 20:27:50 +0800

    chore(win): optimize code for MouseController

--- win/src/mouse.rs
+++ win/src/mouse.rs
@@ -4,15 +4,14 @@ use windows::Win32::UI::Input::KeyboardAndMouse::{
   MOUSEEVENTF_MOVE, MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_RIGHTUP, MOUSEEVENTF_WHEEL, MOUSEINPUT,
   MOUSE_EVENT_FLAGS,
 };
+
 pub struct MouseController {
-  input: INPUT,
-  size: i32,
-  last_mb: MouseButton,
+  last_button_state: MouseButton,
 }
 
 impl MouseController {
-  pub fn new() -> Self {
-    let input = INPUT {
+  const fn empty_mouse_input() -> INPUT {
+    INPUT {
       r#type: INPUT_MOUSE,
       Anonymous: INPUT_0 {
         mi: MOUSEINPUT {
@@ -24,47 +23,56 @@ impl MouseController {
           dwExtraInfo: 0,
         },
       },
-    };
-    let size = std::mem::size_of_val(&input) as i32;
-    let last_mb = MouseButton::default();
-    Self {
-      input,
-      size,
-      last_mb,
     }
   }
 
+  const INPUT_SIZE: i32 = std::mem::size_of_val(&Self::empty_mouse_input()) as i32;
+
+  pub fn new() -> Self {
+    let last_button_state = MouseButton::default();
+    Self { last_button_state }
+  }
+
+  /// Apply the mouse state.
   pub fn apply(&mut self, data: &Mouse) {
-    let mut input = self.input;
-    input.Anonymous.mi.dx = data.x as i32;
-    input.Anonymous.mi.dy = data.y as i32;
-    input.Anonymous.mi.dwFlags.0 = 0;
-    input.Anonymous.mi.mouseData = data.scroll as u32;
+    let mut input = Self::empty_mouse_input();
+
     unsafe {
+      // handle mouse move
       if data.x != 0 || data.y != 0 {
+        input.Anonymous.mi.dx = data.x as i32;
+        input.Anonymous.mi.dy = data.y as i32;
         input.Anonymous.mi.dwFlags.0 |= MOUSEEVENTF_MOVE.0;
       }
-      if data.buttons != self.last_mb {
-        if data.buttons.is_left_button_down() != self.last_mb.is_left_button_down() {
+
+      // handle mouse button
+      if data.buttons != self.last_button_state {
+        if data.buttons.is_left_button_down() != self.last_button_state.is_left_button_down() {
           if data.buttons.is_left_button_down() {
             input.Anonymous.mi.dwFlags.0 |= MOUSEEVENTF_LEFTDOWN.0;
           } else {
             input.Anonymous.mi.dwFlags.0 |= MOUSEEVENTF_LEFTUP.0;
           }
         }
-        if data.buttons.is_right_button_down() != self.last_mb.is_right_button_down() {
+
+        if data.buttons.is_right_button_down() != self.last_button_state.is_right_button_down() {
           if data.buttons.is_right_button_down() {
             input.Anonymous.mi.dwFlags.0 |= MOUSEEVENTF_RIGHTDOWN.0;
           } else {
             input.Anonymous.mi.dwFlags.0 |= MOUSEEVENTF_RIGHTUP.0;
           }
         }
-        self.last_mb = data.buttons;
+
+        self.last_button_state = data.buttons;
       }
+
+      // handle scroll
       if data.scroll != 0 {
+        input.Anonymous.mi.mouseData = data.scroll as u32;
         input.Anonymous.mi.dwFlags.0 |= MOUSEEVENTF_WHEEL.0;
       }
-      SendInput(&[input], self.size);
+
+      SendInput(&[input], Self::INPUT_SIZE);
     }
   }
 }
